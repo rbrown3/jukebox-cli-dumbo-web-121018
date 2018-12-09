e5124d5898783ee254929c38e4481d0e63b12b78
github username <github email address>
2018-12-09 15:23:40 -0500

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +11,27 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+    puts "I accept the following commands:
+    -help : displays this help message
+    -list : displays a list of songs you can play
+    -play : lets you choose a song to play
+    -exit : exits this program"
+    end
+
+
+
+def list (songs)
+    songs.each_with_index do | song, i|
+    puts "#{i + 1} . #{song}"
+    end
+end
+
+def play(songs)
+    songs.include? (element)
+    end
+
+        
+def exit_jukebox
+    puts "Goodbye"
+    end
